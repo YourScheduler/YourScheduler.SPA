76458b4795329ef784f7fc302763c64933363d5a
kUcLis <ulis89@gmail.com>
2023-10-19 23:18:22 +0200

    Added Login page

--- YourScheduler.SPA/src/app/app.module.ts
+++ YourScheduler.SPA/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBodyComponent } from './Components/main-body/main-body.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBodyComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
